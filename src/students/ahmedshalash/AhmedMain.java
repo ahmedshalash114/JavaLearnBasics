1a7a951aa0cdde87a40206427b615827d9235fe8
Ahmed Shalash <ahmedsh@celeritastech.com>
2020-12-27 16:12:47 +0200

    add example package to the project

---
+++ src/students/ahmedshalash/AhmedMain.java
@@ -0,0 +1,11 @@
+package students.ahmedshalash;
+
+public class AhmedMain {
+
+    //start working here
+
+    public static void main(String[] args) {
+        System.out.println("Hello Shalash!");
+    }
+
+}
